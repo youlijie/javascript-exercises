ee644d0dd6d13624e3a427afb308c493a111e581
youlijie <63795226+youlijie@users.noreply.github.com>
2022-06-06 16:58:22 +0200

    1 2 3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 1
+    if(num === 0 || string === "") {
+        return ""
+    } else if(num < 0) {
+        return "ERROR"
+    }
 
+    while (i < num) {
+        string += "hey"
+        i++
+    }
+    return string
 };
 
 // Do not edit below this line
