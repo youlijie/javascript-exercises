2b5de5056bfbc7bf0c9fc0afd9ace42efa80f1ba
youlijie <63795226+youlijie@users.noreply.github.com>
2022-06-08 19:47:03 +0200

    4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const arrays = args[0]
+    return arrays.filter(array => !args.includes(array))
 };
 
 // Do not edit below this line
