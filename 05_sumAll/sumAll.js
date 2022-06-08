a7c21ab79e9a6710ee84423ebcbdb1339e387ebc
youlijie <63795226+youlijie@users.noreply.github.com>
2022-06-08 20:36:25 +0200

    5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
 
+    if(start < 0 || stop < 0 || typeof(start)!=="number" || typeof(stop)!=="number") {
+        return "ERROR"
+    }
+
+    if(start > stop) {
+        const temp = start
+        start = stop
+        stop = temp
+    }
+
+    let sum = 0
+    for(let i = start; i < stop + 1; i++) {
+        sum+= i
+    }
+    return sum
 };
 
 // Do not edit below this line
